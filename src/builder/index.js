5963b45d063536ab2e03d7a22ad9681f03defd1f
tanglei02 <tanglei02@baidu.com>
2018-06-26 14:51:30 +0800

    增加样式自动调试功能

---
+++ src/builder/index.js
@@ -0,0 +1,92 @@
+/**
+ * @file builder server
+ * @author clark-t (clarktanglei@163.com)
+ */
+
+const Koa = require('koa')
+const Router = require('koa-router')
+const path = require('path')
+const webpack = require('webpack')
+const middleware = require('koa-webpack')
+const webpackConfig = require('./webpack.config')
+const fs = require('fs-extra')
+const etpl = require('etpl')
+const {getFilenameFromUrl} = require('webpack-dev-middleware/lib/util')
+
+let etplEngine = new etpl.Engine({
+    commandOpen: '{{',
+    commandClose: '}}',
+    strip: true,
+    namingConflict: 'override',
+    dir: path.resolve(__dirname, './views')
+})
+
+class Server {
+  constructor ({
+    port = 8229
+  } = {}) {
+    this.port = port
+    this.app = new Koa()
+  }
+
+  run () {
+    this.router = new Router()
+    this.router
+      .get('/:id([^\\.]+\\.html)', ...this.html())
+      .get('*', ...this.pack())
+    this.app.use(this.router.routes())
+      .listen(this.port)
+  }
+
+  html () {
+    return [
+      async (ctx, next) => {
+        let filename = path.resolve(__dirname, 'pages', ctx.params.id || 'index.html')
+
+        try {
+          let html = await fs.readFile(filename, 'utf-8')
+          let sidebarHtml = path.resolve(__dirname, 'pages', 'sidebar.html')
+          let cssFilename = getFilenameFromUrl('/', this.compiler, `/index.css`)
+          await this.wait
+          let css = this.midd.devMiddleware.fileSystem.readFileSync(cssFilename, 'utf-8')
+
+          let renderer = etplEngine.loadFromFile(path.resolve(__dirname, './views/layout.tpl'))
+          ctx.body = renderer({
+            css,
+            content: html
+          })
+        }
+        catch (e) {
+          ctx.throw(e)
+          // ctx.throw(404, 'no such file in: ' + filename)
+        }
+
+      }
+    ]
+  }
+
+  pack () {
+    this.compiler = webpack(webpackConfig)
+    this.wait = middleware({
+      compiler: this.compiler,
+      devMiddleware: {
+        publicPath: '/',
+        stats: 'errors-only'
+      },
+      hotClient: false
+    })
+    .then(midd => {
+      this.midd = midd
+    })
+
+    return [
+      async (ctx, next) => {
+        await this.wait
+        await this.midd(ctx, next)
+      }
+    ]
+  }
+}
+
+let app = new Server()
+app.run()
---
+++ src/builder/views/layout.tpl
@@ -0,0 +1,20 @@
+
+<!DOCTYPE html>
+<html mip>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1">
+    <title>MIP page</title>
+    <link rel="canonical" href="对应的原页面地址">
+    <link rel="stylesheet" href="https://bos.nj.bpc.baidu.com/assets/mip/projects/mip.css">
+    <style mip-custom>
+      ${css|raw}
+    </style>
+  </head>
+  <body>
+    <div>
+      ${content|raw}
+    </div>
+    <script src="https://bos.nj.bpc.baidu.com/assets/mip/projects/mip.js"></script>
+  </body>
+</html>
