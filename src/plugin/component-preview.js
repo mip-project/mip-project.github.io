6758d486f8f7a3d6cf7ba38aee048ec7ed92990c
clarkt <clarktanglei@163.com>
2018-09-04 02:59:28 +0800

    增加 edit

--- src/plugin/component-preview.js
+++ src/plugin/component-preview.js
@@ -7,6 +7,127 @@ const path = require('path')
 const fs = require('fs-extra')
 // const cheerio = require('cheerio')
 
+const caseHtml = ({url, style, preset, cases, scripts}) => '' +
+`<!DOCTYPE html>
+<html mip>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+  <title>测试</title>
+  <meta name="description" content="">
+  <link rel="canonical" href="/${url}">
+  <link rel="stylesheet" type="text/css" href="https://bos.nj.bpc.baidu.com/v1/assets/mip/projects/mip.css">
+  ${style}
+</head>
+<body>
+  ${preset}
+  ${cases}
+  <script type="data-x-preset" id="preset">
+  ${preset}
+  </script>
+  <script src="https://bos.nj.bpc.baidu.com/v1/assets/mip/projects/mip.js"></script>
+  ${scripts}
+  <script>
+  var preset;
+  window.addEventListener('message', function (e) {
+    if (!preset) {
+      preset = document.getElementById('preset').innerHTML;
+    }
+    document.body.innerHTML = preset + e.data;
+  })
+  </script>
+</body>
+</html>`
+
+const editHtml = ({cases, url}) => '' +
+`<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
+    <title>测试</title>
+    <meta name="description" content="">
+    <link rel="stylesheet" type="text/css" href="https://cdn.bootcss.com/codemirror/5.38.0/codemirror.min.css">
+    <style>
+      html, body {
+        margin: 0;
+      }
+      .edit-toolbar {
+        height: 60px;
+        padding-left: 20px;
+        line-height: 60px;
+      }
+      #submit {
+        height: 40px;
+        padding-left: 15px;
+        padding-right: 15px;
+      }
+      .edit-wrapper {
+        display: flex;
+        padding: 6px 20px 20px;
+        background: #eee;
+      }
+      .edit-code {
+        flex: 1;
+        overflow: auto;
+      }
+      .edit-iframe-wrapper {
+        width: 320px;
+        margin-left: 20px;
+      }
+      #edit-iframe {
+        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+        background: #fff;
+      }
+      .CodeMirror {
+        box-shadow: 0 1px 2px rgba(0, 0, 0, 0.3);
+        height: 568px;
+      }
+    </style>
+  </head>
+  <body>
+    <div class="edit-toolbar">
+      <button id="submit">点击提交</button>
+    </div>
+    <div class="edit-wrapper">
+      <div class="edit-code">
+        <p class="edit-tip">编辑您的代码：</p>
+        <textarea id="code" name="code">${cases}</textarea>
+      </div>
+      <div class="edit-iframe-wrapper">
+        <p class="edit-tip">查看结果：</p>
+        <iframe id="edit-iframe" frameborder="0" width="320" height="568" src="/${url}"></iframe>
+      </div>
+    </div>
+    <script src="https://cdn.bootcss.com/codemirror/5.38.0/codemirror.min.js"></script>
+    <script src="https://cdn.bootcss.com/codemirror/5.38.0/mode/htmlmixed/htmlmixed.min.js"></script>
+    <script src="https://cdn.bootcss.com/codemirror/5.38.0/addon/selection/active-line.min.js"></script>
+    <script src="https://cdn.bootcss.com/codemirror/5.38.0/addon/edit/matchbrackets.min.js"></script>
+    <script>
+       var editor = CodeMirror.fromTextArea(document.getElementById('code'), {
+        lineNumbers: true,
+        styleActiveLine: true,
+        matchBrackets: true,
+        mode: 'htmlmixed',
+        identUnit: 2
+      });
+      editor.setOption("theme", 'default');
+      var iframe = document.getElementById('edit-iframe');
+      var button = document.getElementById('submit');
+      button.addEventListener('click', function () {
+        let val = editor.getValue();
+        try {
+          iframe.contentWindow.postMessage(val, '*');
+        } catch (e) {
+          console.error('post fail');
+        }
+      });
+    </script>
+  </body>
+</html>
+
+`
+
 module.exports = class ComponentPreview {
   apply (on, app) {
     on(app.STAGES.DONE, async () => {
@@ -43,7 +164,7 @@ module.exports = class ComponentPreview {
           return
         }
 
-        let script = matchScript[1]
+        let scripts = matchScript[1]
           .split(/<br\s*\/?>/)
           .map(url => url.trim())
           .filter(url => /^https?.+?\.js$/.test(url))
@@ -63,20 +184,18 @@ module.exports = class ComponentPreview {
         let fileExtName = path.extname(fileFullPath)
         let fileDirName = path.dirname(fileFullPath)
         let fileName = path.basename(fileBaseName, fileExtName)
-        let demoPreset = path.resolve(fileDirName, 'setting', fileName, 'example.preset')
+        let presetPath = path.resolve(fileDirName, 'setting', fileName, 'example.preset')
 
-        let demoPresetContent
-        if (await fs.exists(demoPreset)) {
-          let file = await fs.readFile(demoPreset, 'utf-8')
-          demoPresetContent = file
+        let preset = ''
+        if (await fs.exists(presetPath)) {
+          let file = await fs.readFile(presetPath, 'utf-8')
+          preset = file
           // demoPresetContent = demoPresetContent.replace(/<[\s\S]+>/g, '')
-        } else {
-          demoPresetContent = ''
         }
 
         let theCases = await Promise.all(
-          matchCases.map(async (theCase, i) => {
-            theCase = theCase
+          matchCases.map(async (cases, i) => {
+            cases = cases
               .replace(/^\s```\s*html\s/, '')
               .replace(/```\s$/, '')
 
@@ -84,36 +203,38 @@ module.exports = class ComponentPreview {
             let height = 568
 
             let caseUrl = obj.url.replace(/^\//, '').replace(/\.html$/, '') + `-case-${i}.html`
+            let editUrl = obj.url.replace(/^\//, '').replace(/\.html$/, '') + `-edit-${i}.html`
 
             let html
+            let edit
 
-            if (/<!DOCTYPE html>/.test(theCase)) {
-              html = theCase
+            if (/<!DOCTYPE html>/.test(cases)) {
+              html = cases
             } else {
-              html = `
-                <!DOCTYPE html>
-                <html mip>
-                <head>
-                  <meta charset="utf-8">
-                  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no, minimal-ui">
-                  <title>测试</title>
-                  <meta name="description" content="">
-                  <link rel="canonical" href="${caseUrl}">
-                  <link rel="stylesheet" type="text/css" href="https://bos.nj.bpc.baidu.com/v1/assets/mip/projects/mip.css">
-                  ${uiStyle}
-                </head>
-                <body>
-                  ${demoPresetContent}
-                  ${theCase}
-                  <script src="https://bos.nj.bpc.baidu.com/v1/assets/mip/projects/mip.js"></script>
-                  ${script}
-                </body>
-              `
+              html = caseHtml({
+                url: caseUrl,
+                style: uiStyle,
+                preset: preset,
+                cases: cases,
+                scripts: scripts
+              })
+
+              edit = editHtml({
+                url: caseUrl,
+                cases: cases
+              })
             }
 
             let pathname = path.resolve(dist, caseUrl)
             await fs.ensureDir(path.dirname(pathname))
             await fs.writeFile(pathname, html, 'utf-8')
+
+            if (edit) {
+              let pathname = path.resolve(dist, editUrl)
+              await fs.ensureDir(path.dirname(pathname))
+              await fs.writeFile(pathname, edit, 'utf-8')
+            }
+
             return `<mip-iframe layout="responsive" width="${width}" height="${height}" src="${app.config.host}/${caseUrl}"></mip-iframe>`
           })
         )
