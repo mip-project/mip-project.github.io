d944a42336632660e0da0259356c7653d26176bb
liuruoran88 <liuruoran88@163.com>
2018-10-11 21:33:22 +0800

    细节修改

--- build.sh
+++ build.sh
@@ -1,42 +1,19 @@
 
 set +e
 
-# 安装包
-echo "-----npm install------"
-npm install
-
-# 编译
-echo "-----pull doc------"
-cd ../mip2
-git pull origin master
-
-# 编译
-echo "-----build------"
-cd ../mip-project.github.io
-npm run build:css
-npm run build
-
-# 杀进程
-echo "-----kill------"
-kill `lsof -t -i:9527`
-
 # 压缩
-# echo "------zip to v2-----"
-# rm -r newV2.zip
-# cd v2
-# zip -r ../newV2.zip ./*
-# cd ..
-
-# bec 云更新
 echo "------zip to v2-----"
-npm run cp:bce
-cd ../mip_online/bec-www.mipengine.org
-git add .
-git commit -am "refresh v2 new"
-git pull origin master
-git push origin master
 
-echo "------ server -----"
-edp webserver
+if [ -s newV3.zip ]; then
+    rm -r newV3.zip
+fi
+
+cd v2
+zip -r -q ../newV3.zip ./*
+cd ..
+
+echo "------上传bos-----"
+
+edp bos newV3.zip bos://assets/mip2/zip
 
 set -e
