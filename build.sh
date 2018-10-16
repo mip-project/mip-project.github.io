94f67ce12de24a9aa719026ff7bb1f41e2b91ef1
liuruoran88 <liuruoran88@163.com>
2018-10-16 14:54:48 +0800

    官网更新脚本

--- build.sh
+++ build.sh
@@ -4,16 +4,16 @@ set +e
 # 压缩
 echo "------zip to v2-----"
 
-if [ -s newV3.zip ]; then
-    rm -r newV3.zip
+if [ -s newMIP2.zip ]; then
+    rm -r newMIP2.zip
 fi
 
 cd v2
-zip -r -q ../newV3.zip ./*
+zip -r -q ../newMIP2.zip ./*
 cd ..
 
 echo "------上传bos-----"
 
-edp bos newV3.zip bos://assets/mip2/zip
+edp bos newMIP2.zip bos://assets/mip2/zip
 
 set -e
